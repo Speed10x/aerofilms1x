789e35222cf705a8d33369c74022063e5d788523
Speed10x <160343394+Speed10x@users.noreply.github.com>
2024-06-21 05:48:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
+git clone https://github.com/Speed10x/aerofilms1x /aerofilms1x
+cd /aerofilms1x
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
